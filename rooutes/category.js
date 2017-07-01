585689504bb0d6d7efff52486d2718a1be483dd1
zhouqs666 <809089952@qq.com>
2017-07-01 16:35:58 +0800

    添加项目依赖和相关模块

---
+++ rooutes/category.js
@@ -0,0 +1,19 @@
+/**
+ * Created by johnson on 2017/7/1.
+ */
+let express = require('express')
+let router = express.Router();
+
+router.get('./list',function (req, res) {
+    res.send('分类列表')
+})
+
+router.get('./add',function (req, res) {
+    res.send('添加分类')
+})
+
+router.get('./delete/:id',function (req, res) {
+    res.send('删除分类')
+})
+
+module.exports=router;
